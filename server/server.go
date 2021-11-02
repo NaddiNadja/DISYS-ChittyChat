c49f34d6000fb70675c1ca24b04c62c1e83510d2
Nadja <nako@itu.dk>
2021-11-02 10:38:33 +0100

    logs added

--- server/server.go
+++ server/server.go
@@ -35,6 +35,8 @@ func (s *chittyChatServiceServer) JoinChannel(ch *chat.Channel, msgStream chat.C
 	msgChannel := make(chan *chat.Message)
 	s.channel[ch.Name] = append(s.channel[ch.Name], msgChannel)
 
+	log.Printf("Client joined")
+
 	// doing this never closes the stream
 	for {
 		select {
@@ -57,6 +59,7 @@ func (s *chittyChatServiceServer) SendMessage(msgStream chat.ChittyChatService_S
 		return err
 	}
 
+	log.Printf("Message recieved from sender \"%v\"", msg.Sender)
 	ack := chat.MessageAck{MessageAck: "SENT"}
 	msgStream.SendAndClose(&ack)
 
