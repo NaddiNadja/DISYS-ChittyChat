44aef14c2846ec60bae89afc8c62c59ecc8f45e2
Nadja <nako@itu.dk>
2021-11-02 14:09:07 +0100

    1. removed hasLeft, 2. removed two flags, 3. changed from Channel to Room

--- client/client.go
+++ client/client.go
@@ -15,11 +15,10 @@ import (
 	"github.com/thecodeteam/goodbye"
 )
 
-var hasLeft = false
 var roomName = flag.String("room", "default", "Chat room for chatting")
 var senderName = flag.String("sender", "default", "Senders name")
-var tcpServer = flag.String("server", ":9080", "Tcp server")
-var lamportTime = flag.Int64("time", 0, "lamportTimeStamp")
+var tcpServer = ":9080"
+var lamportTime = int64(0)
 
 func main() {
 
@@ -29,7 +28,7 @@ func main() {
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithBlock(), grpc.WithInsecure())
 
-	conn, err := grpc.Dial(*tcpServer, opts...)
+	conn, err := grpc.Dial(tcpServer, opts...)
 	if err != nil {
 		log.Fatalf("Fail to dail: %v", err)
 	}
@@ -41,11 +40,11 @@ func main() {
 	defer goodbye.Exit(ctx2, -1)
 	goodbye.Notify(ctx2)
 	goodbye.RegisterWithPriority(func(ctx context.Context, sig os.Signal) {
-		leaveChannel(ctx, client)
+		leaveRoom(ctx, client)
 		conn.Close()
 	}, 0)
 
-	go joinChannel(ctx, client)
+	go joinRoom(ctx, client)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -54,22 +53,18 @@ func main() {
 
 }
 
-func joinChannel(ctx context.Context, client chat.ChittyChatServiceClient) {
+func joinRoom(ctx context.Context, client chat.ChittyChatServiceClient) {
 
-	channel := chat.Room{Name: *roomName, SendersName: *senderName}
-	stream, err := client.JoinRoom(ctx, &channel)
+	room := chat.Room{Name: *roomName, SendersName: *senderName}
+	stream, err := client.JoinRoom(ctx, &room)
 	if err != nil {
-		log.Fatalf("client.JoinChannel(ctx, &channel) throws: %v", err)
+		log.Fatalf("client.JoinRoom(ctx, &room) throws: %v", err)
 	}
 	sendMessage(ctx, client, "This user just joined.")
 
 	waitc := make(chan struct{}) //go never stops with this
 	go func() {
 		for {
-			if hasLeft {
-				close(waitc)
-				return
-			}
 			in, err := stream.Recv()
 			if err == io.EOF {
 				close(waitc)
@@ -79,12 +74,12 @@ func joinChannel(ctx context.Context, client chat.ChittyChatServiceClient) {
 				log.Fatalf("Failed to receive message from channel joining. \nErr: %v", err)
 			}
 			if *senderName != in.Sender {
-				if in.LamportTime > *lamportTime {
-					*lamportTime = in.LamportTime + 1
+				if in.LamportTime > lamportTime {
+					lamportTime = in.LamportTime + 1
 				} else {
-					*lamportTime++
+					lamportTime++
 				}
-				fmt.Printf("(%v) %v: %v \n", *lamportTime, in.Sender, in.Message)
+				fmt.Printf("(%v) %v: %v \n", lamportTime, in.Sender, in.Message)
 			}
 		}
 	}()
@@ -92,15 +87,15 @@ func joinChannel(ctx context.Context, client chat.ChittyChatServiceClient) {
 
 }
 
-func leaveChannel(ctx context.Context, client chat.ChittyChatServiceClient) {
+func leaveRoom(ctx context.Context, client chat.ChittyChatServiceClient) {
 	sendMessage(ctx, client, "just left in the hardcore way")
 	log.Println("Ses b")
-	channel := chat.Room{Name: *roomName, SendersName: *senderName}
-	client.LeaveRoom(ctx, &channel)
+	room := chat.Room{Name: *roomName, SendersName: *senderName}
+	client.LeaveRoom(ctx, &room)
 }
 
 func sendMessage(ctx context.Context, client chat.ChittyChatServiceClient, message string) {
-	*lamportTime++
+	lamportTime++
 	stream, err := client.SendMessage(ctx)
 	if err != nil {
 		log.Printf("Cannot send message: error: %v", err)
@@ -111,7 +106,7 @@ func sendMessage(ctx context.Context, client chat.ChittyChatServiceClient, messa
 			SendersName: *senderName},
 		Message:     message,
 		Sender:      *senderName,
-		LamportTime: *lamportTime,
+		LamportTime: lamportTime,
 	}
 	stream.Send(&msg)
 }
